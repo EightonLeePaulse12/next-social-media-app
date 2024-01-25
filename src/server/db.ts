b3dca84bdc2ae991256db2557887901d85c9106d
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2024-01-25 13:11:05 +0200

    fixed

--- src/server/db.ts
+++ src/server/db.ts
@@ -20,7 +20,7 @@ export const db =
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaPlanetScale(client) as PrismaAdapter,  
+    adapter: new PrismaPlanetScale(client) as any,  
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
